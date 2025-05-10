50ee70d28498f6aeabc269d9b8d2c23a7716c53a
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-10 02:58:26 +0100

    modification du fichier create_durectory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Ce script crée un dossier nommé test_directory
-mkdir -p test_directory
-echo "Le dossier 'test_directory' a été créé"
+mkdir -p test_directoire
+echo "Le dossier 'test_directoire' a été créé"
