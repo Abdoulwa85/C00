64391f9497eefc89e37c3b8d9b9d124901f717eb
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-10 01:22:32 +0100

    modification et renomage du fichier check_file.sh en check_file_exist.sh

---
+++ check_file_exist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Ce script vérifie si un fichier existe
+
+echo "Entrez le nom du fichier : "
+read filename
+
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe"
+else
+    echo "Le fichier '$filename' n'existe pas"
+fi
+
