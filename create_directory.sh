c65a00f64fa593092d3ec891d38bffc30f051a9a
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-10 02:33:43 +0100

    corrections des messages d'affichages

--- create_directory.sh
+++ create_directory.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Ce script crée un dossier nommé test_directory
 mkdir -p test_directory
-echo "le dossier 'test_directory' a été crée"
+echo "Le dossier 'test_directory' a été créé"
--- list_files.sh
+++ list_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # ce script permet d'afficher tous les fichiers du dossier actuel
-echo "Voici la liste des fichier du dossie courant"
+echo "Voici la liste des fichiers du dossier courant"
 ls -p | grep -v /
